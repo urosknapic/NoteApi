7c42db81ff56630988c6745b2b5b43ca88523ebd
urosknapic <uros.knapic@ceneje.si>
2021-10-28 20:12:31 +0200

    Create Notes controller

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesControllers : ControllerBase
+    {
+
+    }
+}
