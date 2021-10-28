3a7a1c0aeab8b2a60df2cbd08965db63af0d677d
urosknapic <uros.knapic@ceneje.si>
2021-10-28 20:56:25 +0200

    Add scope for dependency injection

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NoteApi.Data;
 
 namespace NoteApi
 {
@@ -32,6 +33,8 @@ namespace NoteApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NoteApi", Version = "v1" });
             });
+
+            services.AddScoped<INoteRepository, NoteRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
