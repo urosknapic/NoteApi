2c2383dcd10e01fc28a44ed67130e812e9cf089c
urosknapic <uros.knapic@ceneje.si>
2021-11-01 12:12:07 +0100

    Add update dto for folder

--- Controllers/FoldersController.cs
+++ Controllers/FoldersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using NoteApi.Data;
 using NoteApi.Data.Tables;
@@ -36,7 +37,7 @@ namespace NoteApi.Controllers
         [HttpGet("{id}", Name = "GetFolderById")]
         public ActionResult GetFolderById(int id)
         {
-            var folderItem = _repository.GetFolderById(id);
+            Folder folderItem = _repository.GetFolderById(id);
 
             if (folderItem == null)
             {
@@ -46,20 +47,78 @@ namespace NoteApi.Controllers
             return Ok(_mapper.Map<FolderReadDto>(folderItem));
         }
 
-        
+
         [HttpPost]
         public ActionResult<FolderReadDto> CreateFolder(FolderCreateDto createDto)
         {
-            var folder = _mapper.Map<Folder>(createDto);
-            folder.CreatedAt = DateTime.Now;
+            Folder folderItem = _mapper.Map<Folder>(createDto);
+            folderItem.CreatedAt = DateTime.Now;
 
-            _repository.CreateFolder(folder);
+            _repository.CreateFolder(folderItem);
             _repository.SaveChanges();
 
-            var folderDto = _mapper.Map<FolderReadDto>(folder);
+            var folderDto = _mapper.Map<FolderReadDto>(folderItem);
 
             return CreatedAtRoute("GetFolderById", new { Id = folderDto.Id }, folderDto);
         }
 
+        [HttpPut("{id}")]
+        public ActionResult UpdateFolder(int id, FolderUpdateDto updateDto)
+        {
+            Folder folderItem = _repository.GetFolderById(id);
+
+            if (folderItem == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updateDto, folderItem);
+
+            _repository.UpdateFolder(folderItem);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+        [HttpPatch("{id}")]
+        public ActionResult PartialNoteUpdate(int id, JsonPatchDocument<FolderUpdateDto> patchNote)
+        {
+            Folder folderItem = _repository.GetFolderById(id);
+
+            if (folderItem == null)
+            {
+                return NotFound();
+            }
+
+            var folderUpdatePatch = _mapper.Map<FolderUpdateDto>(folderItem);
+            patchNote.ApplyTo(folderUpdatePatch, ModelState);
+
+            if (!TryValidateModel(folderUpdatePatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(folderUpdatePatch, folderItem);
+
+            _repository.UpdateFolder(folderItem);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteNote(int id)
+        {
+            Folder folderItem = _repository.GetFolderById(id);
+
+            if (folderItem == null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeleteFolder(folderItem);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
--- Profilers/FolderProfilerMapper.cs
+++ Profilers/FolderProfilerMapper.cs
@@ -10,8 +10,8 @@ namespace NoteApi.Profilers
         {
             CreateMap<Folder, FolderReadDto>();
             CreateMap<FolderCreateDto, Folder>();
+            CreateMap<FolderUpdateDto, Folder>();
             /* 
-            CreateMap<NoteUpdateDto, Note>();
             CreateMap<Note, NoteUpdateDto>();
              */
         }
